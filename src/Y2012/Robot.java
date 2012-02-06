8d0f71868ff8bc827dd8cf9232bffdba095451a7
Colin Poler <colin_poler@asl.org>
2012-02-06 19:01:22 +0000

    Started wiring

--- src/Y2012/Robot.java
+++ src/Y2012/Robot.java
@@ -7,11 +7,11 @@ import driveSystems.*;
 import _static.AbstractRobot;
 
 public class Robot extends AbstractRobot {
-	public static final int SHOOTER = 0;
+	public static final ShootingApparatus shootingApparatus;
+	static {shootingApparatus = new ShootingApparatus(Wiring.shootingMotors, Wiring.shootingMotorCoefficients);}
 	public static Robot robot() {
-		DriveSystem driveSystem = new CaliforniaDrive(Wiring.motors, Wiring.motorCoefficients);
-		Apparatus[] apparatuses = new Apparatus[1];
-		apparatuses[SHOOTER] = new ShootingApparatus();
+		DriveSystem driveSystem = new CaliforniaDrive(Wiring.driveMotors, Wiring.drivingMotorCoefficients);
+		Apparatus[] apparatuses = new Apparatus[] {shootingApparatus};
 		return new Robot(driveSystem, apparatuses);
 	}
 	public Robot(DriveSystem driveSystem, Apparatus[] apparatuses) {
--- src/Y2012/shooting/ShootingApparatus.java
+++ src/Y2012/shooting/ShootingApparatus.java
@@ -1,7 +1,14 @@
 package Y2012.shooting;
 
+import edu.wpi.first.wpilibj.Jaguar;
 import _static.Apparatus;
 
 public class ShootingApparatus extends Apparatus {
-	
+	public static final int CONVEYOR = 0, SHOOTING = 2, LOWER = 0, UPPER = 1, POWER = 0, ANGLE = 1;
+	public final Jaguar[] motors;
+	public final double[] motorCoefficients;
+	public ShootingApparatus(Jaguar[] motors, double[] motorCoefficients) {
+		this.motors = motors;
+		this.motorCoefficients = motorCoefficients;
+	}
 }
