d79697f62a935a37db29abe3a58b6a31fb1bc6e9
Colin Poler <colin_poler@asl.org>
2012-02-16 14:44:45 +0000

    Revert "test for sensor listener"
    This reverts commit bc24302186d72d38ceb8178897d09dcdcfacc51f.

--- src/Y2012/bridge/Monodent.java
+++ src/Y2012/bridge/Monodent.java
@@ -1,20 +1,17 @@
 package Y2012.bridge;
 
 import _static.Apparatus;
-import sensors.BooleanSensor.BooleanSensorEvent;
 import sensors.LimitSwitch;
-import sensors.LimitSwitch.LimitSwitchListener;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Relay.Value;
 
-public class Monodent extends Apparatus implements LimitSwitchListener {
+public class Monodent extends Apparatus {
 	protected final Relay motor;
 	public final LimitSwitch frontSwitch, backSwitch;
 	public Monodent(Relay motor, LimitSwitch frontSwitch, LimitSwitch backSwitch) {
 		this.motor = motor;
 		this.frontSwitch = frontSwitch;
 		this.backSwitch = backSwitch;
-		this.frontSwitch.addListener(this);
 	}
 	public void down() {
 		if(!frontSwitch.value()) {
@@ -28,9 +25,5 @@ public class Monodent extends Apparatus implements LimitSwitchListener {
 	}
 	public void off() {
 		motor.set(Value.kOff);
-	}
-	private int i;
-	public void limitSwitch(BooleanSensorEvent ev) {
-		System.out.println("lolololol " + ev.currentValue + " " + i++);
 	}
 }
