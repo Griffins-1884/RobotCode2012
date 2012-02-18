2c02d804f3a50dd57de269f1be735b284530f0a6
Colin Poler <colin_poler@asl.org>
2012-02-18 13:25:54 +0000

    Dashboard can send locations

--- src/communications/Dashboard.java
+++ src/communications/Dashboard.java
@@ -1,10 +1,18 @@
 package communications;
 
+import spatial.Location;
 import edu.wpi.first.wpilibj.SmartDashboard;
 
 public class Dashboard {
 	static {
 		SmartDashboard.init();
+	}
+	public void displayLocation(Location location, double rotation) {
+		String value = "{x:" + location.x + "," + 
+						"y:" + location.y + "," + 
+						"z:" + location.z + "," + 
+						"r:" + rotation + "}";
+		SmartDashboard.log(value, "__location__");
 	}
 	public void overlay(int x, int y, int height, int width, int color) {
 		String value = "{x:" + x + "," + 
